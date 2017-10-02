2d35267e3ca0cf27e30614ea55ff93c9252ee410
Tony Butler <tonyjbutler@gmail.com>
2017-10-02 09:59:55 +0100

    mod_leganto: Add empty upgrade.php to keep Moodle code validator happy

---
+++ db/upgrade.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Leganto module upgrade code.
+ *
+ * @package    mod_leganto
+ * @copyright  2017 Lancaster University {@link http://www.lancaster.ac.uk/}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author     Tony Butler <a.butler4@lancaster.ac.uk>
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Upgrade the leganto module to the current version.
+ *
+ * @param int $oldversion The old version of the leganto module.
+ * @return bool True.
+ */
+function xmldb_leganto_upgrade($oldversion) {
+    global $CFG, $DB;
+
+    $dbman = $DB->get_manager();
+
+    // Moodle v3.1.0 release upgrade line.
+    // Put any upgrade step following this.
+
+    return true;
+}
